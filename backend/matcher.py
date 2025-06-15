7ba9396058e495e7b8dbbadfd00ae16ead328148
Rohan Venkatesha <149955518+rohanvenkatesha@users.noreply.github.com>
2025-06-15 15:32:21 -0400

    updated spacy

--- backend/matcher.py
+++ backend/matcher.py
@@ -1,7 +1,11 @@
 import spacy
 from typing import List, Dict
 
-nlp = spacy.load("en_core_web_sm")
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+
+
+# nlp = spacy.load("en_core_web_sm")
 
 def extract_skill_phrases(text: str) -> List[str]:
     doc = nlp(text.lower())
